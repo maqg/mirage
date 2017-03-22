bd78ec034549f0ff7001dc3d01586bd651d88cab
maqg <maquangang@163.com>
2017-03-22 23:09:14 +0800

    maqg for entry function

--- frontend/ui/app/entry.component.ts
+++ frontend/ui/app/entry.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component ({
-    selector: 'mirage-ui',
+    selector: 'ez-app',
     templateUrl: './app/entry.component.html'
 })
 export class EntryComponent {
