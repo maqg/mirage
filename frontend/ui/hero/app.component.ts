d96fd39107cce223dbc3dea5a4b1113df5d70dc3
maqg <maquangag@163.com>
2017-03-23 17:53:22 +0800

    maqg for hero storage function updating

--- frontend/ui/hero/app.component.ts
+++ frontend/ui/hero/app.component.ts
@@ -13,20 +13,4 @@ import { HeroService } from './hero.service';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
--- frontend/ui/hero/hero-detail.component.ts
+++ frontend/ui/hero/hero-detail.component.ts
@@ -1,10 +1,11 @@
 import 'rxjs/add/operator/switchMap';
+
 import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location }               from '@angular/common';
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
 
-import { Hero }         from './hero';
-import { HeroService }  from './hero.service';
 @Component({
   selector: 'my-hero-detail',
   templateUrl: './hero/hero-detail.component.html',
@@ -12,19 +13,20 @@ import { HeroService }  from './hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
-
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
-
   ngOnInit(): void {
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
       .subscribe(hero => this.hero = hero);
   }
-
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
   goBack(): void {
     this.location.back();
   }
