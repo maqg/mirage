6f96dbb1aa89af115b3eb1555a23a533f047f214
maqg <maquangang@163.com>
2017-06-12 23:33:06 +0800

    change angular to version 2

--- frontend/ng/main.ts
+++ frontend/ng/main.ts
@@ -6,5 +6,3 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-    .then(success => console.log('BootStrap success'))
-    .catch(error => console.log(error))
