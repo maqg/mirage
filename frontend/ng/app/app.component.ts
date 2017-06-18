3677ecd9d694bddff6217c3c9b7820496a64d18c
maqg <maquangang@163.com>
2017-06-18 15:15:36 +0800

    for ng header of content

--- frontend/ng/app/app.component.ts
+++ frontend/ng/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'mirage-ui',
     providers: [],
+    styleUrls: [ './app/app.component.css' ],
     templateUrl: 'app/app.component.html',
 })
 export class AppComponent implements OnInit {
