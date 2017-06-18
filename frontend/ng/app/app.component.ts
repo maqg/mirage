a683e85a5c2cc3d16b15e4e6ee385babd7295114
maqg <maquangang@163.com>
2017-06-18 16:33:28 +0800

    for account management

--- frontend/ng/app/app.component.ts
+++ frontend/ng/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Account } from './account';
 
 import { Component, OnInit } from '@angular/core';
 
@@ -10,7 +11,18 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     private where : string;
 
+    private accounts: Account[];
+
     ngOnInit() {
         this.where = "China";
+
+        this.accounts = [
+            { id: 1, name: "admin" },
+            { id: 2, name: "root" },
+            { id: 3, name: "henry" },
+            { id: 4, name: "jacky" },
+            { id: 5, name: "bruce" },
+        ];
+
     }
 }
--- frontend/ng/app/app.module.ts
+++ frontend/ng/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core'
 
 import { BrowserModule } from '@angular/platform-browser'
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 @NgModule({
     imports: [
         BrowserModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
     ],
 
     declarations: [
         AppComponent
     ],
+
     providers: [],
     bootstrap: [
         AppComponent
     ],
 })
-export class AppModule {}
+export class AppModule { }
